0a2b9cc1c3022777822dea0209fb640d16cfd6b1
chenxiaonan-magicbook <1093550440@qq.com>
2025-07-14 21:27:12 +0800

    对顶堆

---
+++ src/main/java/dataStructure/heap/luogu/p7072/Main.java
@@ -0,0 +1,65 @@
+package com.cxn.dataStructure.heap.p7072;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/**
+ *   对顶堆
+ *   https://www.luogu.com.cn/problem/P7072
+ */
+public class Main {
+    static int n, w, score, k;
+    static PriorityQueue<Integer> minQueue, maxQueue;
+
+    public static void main(String[] args) {
+        minQueue = new PriorityQueue<>((a, b) -> a - b);
+        maxQueue = new PriorityQueue<>((a, b) -> b - a);
+        FastScanner sc = new FastScanner();
+        n = sc.nextInt();
+        w = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            score = sc.nextInt();
+            if (minQueue.isEmpty() || score >= minQueue.peek()) {
+                minQueue.offer(score);
+            } else {
+                maxQueue.offer(score);
+            }
+            k = Math.max(1, (i + 1) * w / 100);
+            while (minQueue.size() > k) {
+                maxQueue.offer(minQueue.poll());
+            }
+            while (!maxQueue.isEmpty() && minQueue.size() < k) {
+                minQueue.offer(maxQueue.poll());
+            }
+            sb.append(minQueue.peek()).append(" ");
+        }
+        if (sb.length() > 0) {
+            sb.setLength(sb.length() - 1);
+        }
+        System.out.println(sb);
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
